6535778c90aa5a0679c153625f5a042e81713ebc
arthurscreative <arthur.silva@rutgers.edu>
2020-02-03 20:51:38 -0500

    Muse movement script

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  void Update()
     {
-        
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
